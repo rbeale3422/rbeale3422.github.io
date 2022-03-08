b183aba443683ee35f806b8cb957273b8a1d7f24
mscdaddy30 <mscdaddy30@gmail.com>
2022-03-08 09:20:04 -0600

    first upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function PrintDate() {
+    var month = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth();
+    var currentMonth = month[mm];
+    var yyyy = today.getFullYear();
+    today = currentMonth + ' ' + dd + ', ' + yyyy;
+    document.getElementById('date').innerText = today;
+    //console.log(date)
+}
